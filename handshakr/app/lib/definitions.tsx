2201df10deaf433d73721973fe02fbf8776ec093
aashpis <37486859+aashpis@users.noreply.github.com>
2025-02-23 18:58:29 -0500

    added HandshakeFormSchema

--- handshakr/app/lib/definitions.tsx
+++ handshakr/app/lib/definitions.tsx
@@ -46,7 +46,7 @@ export type FormState =
   export type SessionPayload = {
     userId: 'id',
     role: 'role',
-    token: 'token'    
+    token: 'token',    
   }
 
   // stores results from API calls. 
@@ -68,3 +68,23 @@ export type FormState =
       role?: string
     }
   }
+
+
+
+  // validate data from sign up.
+export const HandshakeFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: 'Name must be at least 2 characters long.' })
+    .trim(),
+  email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
+  password: z
+    .string()
+    .min(8, { message: 'Be at least 8 characters long' })
+    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+    .regex(/[0-9]/, { message: 'Contain at least one number.' })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: 'Contain at least one special character.',
+    })
+    .trim(),
+})
