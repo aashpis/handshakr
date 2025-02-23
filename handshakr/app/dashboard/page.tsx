8394b969837b1f84fc80fbd2b22fcde4190cb7f9
aashpis <37486859+aashpis@users.noreply.github.com>
2025-02-23 18:57:26 -0500

    added TODO

--- handshakr/app/dashboard/page.tsx
+++ handshakr/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
+import { verifySession } from './app/lib/dal'
+
+
+
 export default function Page() {
     return <p>My profile page</p>;
   }
+ 
+
+// TODO: implement user vs admin dashboard 
+// export default function Dashboard() {
+//   const session = await verifySession()
+//   const userRole = session?.user?.role // Assuming 'role' is part of the session object
+ 
+//   if (userRole === 'admin') {
+//     return <AdminDashboard />
+//   } else if (userRole === 'user') {
+//     return <UserDashboard />
+//   } else {
+//     redirect('/login')
+//   }
+// }
