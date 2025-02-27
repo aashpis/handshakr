041f0532129ef4b57e77efb1de11b34db68f6e04
aashpis <37486859+aashpis@users.noreply.github.com>
2025-02-26 22:30:40 -0500

    init commit

---
+++ handshakr/app/ui/profile-card.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function ProfileCard({username, userId, email}){
+   <div>
+    <h1>My Profile</h1>
+    <p>username: {username}</p>
+    <p>user ID: {userId}</p>
+    <p>email: {email}</p>
+   </div>
+}
