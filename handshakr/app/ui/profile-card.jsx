d786d05f43b4d0315e3fc98db2d0df56e136aaca
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-01 12:03:20 -0500

    styling

--- handshakr/app/ui/profile-card.jsx
+++ handshakr/app/ui/profile-card.jsx
@@ -1,11 +1,17 @@
 
 
 
-export default function ProfileCard({username, userId, email}){
-   <div>
-    <h1>My Profile</h1>
-    <p>username: {username}</p>
-    <p>user ID: {userId}</p>
-    <p>email: {email}</p>
-   </div>
+export default function ProfileCard({ username, userId, email }) {
+   return (
+      <div className="bg-white my-4 text-grey-700 text-sm border rounded">
+         <div className="m-4">
+            <h1 className="text-blue-500">My Profile</h1>
+            <p>username: <span className="text-blue-700 font-bold">{username}</span></p>
+            <p>user ID: <span className="text-blue-700 font-bold">{userId}</span></p>
+            <p>email: <span className="text-blue-700 font-bold">{email}</span></p>
+         </div>
+      </div>
+
+      
+   );
 }
