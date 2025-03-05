fc9e1cd82c87aa1c7b41640aa0609143127a3c4e
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-05 00:14:26 -0500

    removed handshakeform

--- handshakr/app/dashboard/page.tsx
+++ handshakr/app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 import { verifySession } from './app/lib/dal'
 import  ProfileCard  from '../ui/profile-card'
 import HandshakeCreationForm from '../ui/handshake-creation-form'
+import { getUserProfile } from '@/lib/dal'
 
 const user = {
   username: "jSmith",
   userID: "123456",
   email: "jsmith@gmail.com"
 }
+
+const userProfile = getUserProfile() 
 
 export default function Page() {
     return (
@@ -16,7 +19,7 @@ export default function Page() {
     userId = {user.userID}
     email = {user.email}
     />
-    <HandshakeCreationForm />
+
     </div>
     );
   }
