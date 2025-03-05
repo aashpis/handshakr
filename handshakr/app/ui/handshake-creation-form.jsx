6dea08c4ab401389483991008c726c162a501134
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-05 00:20:22 -0500

    formatting

--- handshakr/app/ui/handshake-creation-form.jsx
+++ handshakr/app/ui/handshake-creation-form.jsx
@@ -10,41 +10,76 @@ export default function HandshakeCreationForm() {
     <form action={action}>
       
       {/* add title */}
-      <div>
-        <label htmlFor="name">Handshake Name</label>
-        <input id="name" name="name" placeholder="My Handshake..." />
+      <div className="mb-4">
+        <label 
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="name">
+          Handshake Name
+        </label>
+        <input 
+        
+        id="name" 
+        name="name" 
+        placeholder="My Handshake..." />
       </div>
       {state?.errors?.name && <p>{state.errors.name}</p>}
  
       {/* add compensation */}
-      <div>
-        <label htmlFor="compensation">Compensation</label>
-        <input id="compensation" name="compensation" placeholder="compensation" />
+      <div className="mb-4">
+        <label 
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="compensation">Compensation</label>
+        <input 
+        id="compensation" 
+        name="compensation" 
+        placeholder="compensation" />
       </div>
       {state?.errors?.email && <p>{state.errors.email}</p>}
      
       {/* add terms of handshake */}
-      <div>
-        <label htmlFor="terms">Terms</label>
-        <input type="textarea" id="terms" name="terms" placeholder="the terms of this handshake..." />
+      <div className="mb-4">
+        <label 
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="terms">Terms</label>
+        <input 
+        type="textarea" 
+        id="terms" 
+        name="terms" 
+        placeholder="the terms of this handshake..." />
       </div>
       {state?.errors?.email && <p>{state.errors.email}</p>}
 
       {/* add date of handshake */}
-      <div>
-        <label htmlFor="date">Date of Execution</label>
-        <input type="date" id="date" name="date" placeholder="compensation" />
+      <div className="mb-4">
+        <label 
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="date">Date of Execution</label>
+        <input 
+        
+        type="date" 
+        id="date" 
+        name="date" 
+        placeholder="compensation" />
       </div>
       {state?.errors?.email && <p>{state.errors.email}</p>}
 
       {/* add agreerer to handshake
           TODO: how to connect agreerer? User Id
       */}
-      <button type="button" onClick={ connectAgreer }>Add Agreerer to Shake</button>
-      
-      <button disabled={pending} type="submit">
-        Create Handshake
+      <button 
+      className="bg-blue-500 hover:bg-blue-700 text-white p-1  rounded focus:outline-none focus:shadow-outline"
+      type="button" 
+      onClick={ connectAgreer }>
+        Add Agreerer to Shake
       </button>
+      <div className="m-2">
+        <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        disabled={pending} 
+        type="submit">
+          Create Handshake
+        </button>
+      </div>
     </form>
   )
 }
