fd1d184846b9c16e6940c6b780f728206b8170d4
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-06 21:57:52 -0500

    added components to page

--- handshakr/app/handshakes/page.tsx
+++ handshakr/app/handshakes/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 import { useState } from "react";
 import HandshakeCreationForm from "../ui/handshake-creation-form";
+import HandshakeCard from "../ui/handshake-card"
 
-
+const handshake = {
+    id: "12345",
+    initiator: "Shankar",
+    agreer: "Ari",
+    compensation: "$20",
+    terms: "I will sell you a used computer mouse for $20",
+    status: "pending",
+    notarized: false,
+}
 
 export default function Page() {
-return <p>Here are your active Handshakes</p>
+    return (<div className="flex flex-col items-center justify-top min-h-screen">
+
+        <HandshakeCard {...handshake} />
+    </div>
+
+    );
 }
