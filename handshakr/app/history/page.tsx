845332053bec14bd422755d5f4cd60ff73d27a3b
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-06 21:58:05 -0500

    added components

--- handshakr/app/history/page.tsx
+++ handshakr/app/history/page.tsx
@@ -1,3 +1,65 @@
-export default function Page() {
-    return <p>My history page</p>;
-  }
+"use client";
+import { useState } from "react";
+import HandshakeCreationForm from "../ui/handshake-creation-form";
+import HandshakeCard from "../ui/handshake-card"
+
+const handshakes = [
+    {
+        id: "12345",
+        initiator: "Shankar",
+        agreer: "Ari",
+        compensation: "$20",
+        terms: "I will sell you a used computer mouse for $20",
+        status: "completed",
+        notarized: false,
+    },
+    {
+        id: "12346",
+        initiator: "Lenny",
+        agreer: "Mike",
+        compensation: "Lunch",
+        terms: "I will help you move furniture in exchange for lunch",
+        status: "completed",
+        notarized: false,
+    },
+    {
+        id: "12347",
+        initiator: "Jordan",
+        agreer: "Casey",
+        compensation: "$50",
+        terms: "I will design a logo for your startup for $50",
+        status: "completed",
+        notarized: "John Doe",
+    },
+    {
+        id: "12348",
+        initiator: "Ari",
+        agreer: "Mekky",
+        compensation: "Takis",
+        terms: "I will give you a bag Takis for a pizza",
+        status: "failed",
+        notarized: false,
+    },
+    {
+        id: "12349",
+        initiator: "Sam",
+        agreer: "Morgan",
+        compensation: "$100",
+        terms: "I will develop a simple website for you for $100",
+        status: "completed",
+        notarized: "Jane Smith",
+    }
+];
+
+
+
+
+    export default function Page() {
+        return (
+            <div className="flex flex-col items-center justify-top min-h-screen">
+                {handshakes.map((handshake) => (
+                    <HandshakeCard key={handshake.id} {...handshake} />
+                ))}
+            </div>
+        );
+    }
