0a4a62f20438f2324fe1982fe80cc224b47ba7b0
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-16 18:13:19 -0400

    styling

--- handshakr/app/ui/sidenav.jsx
+++ handshakr/app/ui/sidenav.jsx
@@ -6,7 +6,7 @@ import NavLinks from './nav-links';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col bg-neutral-light px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-center p-4 md:h-40 bg-primary"
         href="/"
@@ -21,11 +21,11 @@ export default function SideNav() {
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="hidden h-auto w-full grow rounded-md md:block"></div>
         {/* Remove the form and use Link directly */}
         <Link
           href="/"
-          className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-red-500 hover:text-gray-50 md:flex-none md:justify-start md:p-2 md:px-3"
+          className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-primary-light text-primary-dark text-bold p-3 text-sm font-medium hover:bg-red-500 hover:text-gray-50 md:flex-none md:justify-start md:p-2 md:px-3"
         >
           {/* <PowerIcon className="w-6" /> */}
           <div className="hidden md:block">Sign Out</div>
