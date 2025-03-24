0fcfc436e7499f8078b2574a5fa869246ab4053a
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-24 10:22:20 -0400

    added functions using axios api

--- handshakr/app/lib/dal.ts
+++ handshakr/app/lib/dal.ts
@@ -3,73 +3,47 @@
 'use server' 
 
 import 'server-only'
-import { cookies } from 'next/headers'
-import { decrypt } from './session'
-import { cache} from 'react'
-import { redirect } from 'next/navigation'
-import {API, ApiResult} from './definitions'
+import {API} from './definitions'
+import axiosInstance from "./axiosInstance";
 
-// if verified: stores isAuth and userId in cache
-// else: redirect to home page
-export const verifySession = cache(async () => {
-  const cookie = (await cookies()).get('session')?.value
-  const session = await decrypt(cookie)
- 
-  if (!session?.userId) {
-    redirect('/')
-  }
- 
-  return { isAuth: true, userId: session.userId } //TODO: decide what needs to be stored in the cache
-})
+
+// let data = { username: string; userID: string }
 
 // get user profile data
-
-
-
-  // check if email is available to use for signup
-export async function getUserProfile(): Promise<ApiResult<boolean>> {
-    try {
-      const response = await fetch(API.USER_PROFILE, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-  
-      if (!response.ok) {
-        return {
-          success: false,
-          error: 'Failed to get user profile data'
-        }
-      }
-  
-      const authResponse = await response.json();
-      return {
-        success: true,
-        data: authResponse
-      };
-    } catch (error) {
-        return {
-          success: false,
-          error: error instanceof Error ? error.message : 'An unexpected error occurred'
-        };
-      }
+export async function getUserProfile(userId: string ) {
+  try {
+    const response = await axiosInstance.get(`${API.USER_PROFILE}${userId}`);
+    return { success: true, data: response.data };
+  } catch (error: any) {
+    return { success: false, error: error.response?.data?.message || "Failed to get profile" }; 
   }
-
-// edit user profile data
-export async function editUser(){
-
 }
+
+
+
 
 // show which handshakes have not been completed
-export async function getPendingHandshakes(){
-
+export async function getPendingHandshakes(userId: string ){
+  try {
+    const response = await axiosInstance.get(`${API.USER_PROFILE}${userId}`);
+    return { success: true, data: response.data };
+  } catch (error: any) {
+    return { success: false, error: error.response?.data?.message || "Failed to get your pending handshakes" }; 
+  }
 }
+
+
 
 
 // get encrypted handshake history data
 // each handshake must be decrypted by user to view
-export async function getHandshakeHistory(){
+export async function getHandshakeHistory(data: { username: string; password: string }){
+  try {
+    const response = await axiosInstance.get(API.HANDSHAKE.HISTORY, data);
+    return { success: true, data: response.data };
+  } catch (error: any) {
+    return { success: false, error: error.response?.data?.message || "Failed to get handshakes history" }; 
+  }
 
 }
 
@@ -78,19 +52,16 @@ export async function decryptHandshake(){
 
 }
 
-// creates a handshake 
-export async function createHandshake(){
-  console.log("handhshake created")
-  return true
 
-}
 
-// connect agreerer to new handshake
-export async function connectAgreer(){
-  return true
 
-}
-
-export async function acceptHandshake(){
+// connect agreererer to new handshake
+export async function connectagreerer(handshakeId: string, userId: string){
+  try {
+      const response = await axiosInstance.post(API.HANDSHAKE.CONNECT_agreererER, handshakeId);
+      return { success: true, data: response.data };
+    } catch (error: any) {
+      return { success: false, error: error.response?.data?.message || "failed to reject handshake" }; 
+    }
 
 }
